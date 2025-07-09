c84375d46446824a78e3a2a35465a54b1770a125
Thien Nguyen <thienn@outlook.com>
2025-07-09 13:53:52 +0700

    use Agent APIs

---
+++ NTG.Agent.Orchestrator/Plugins/DateTimePlugin.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace NTG.Agent.Orchestrator.Plugins;
+
+sealed class DateTimePlugin
+{
+    [KernelFunction, Description("Get current datetime")]
+    public DateTime GetCurrentDateTime()
+    {
+        return DateTime.Now;
+    }
+}
