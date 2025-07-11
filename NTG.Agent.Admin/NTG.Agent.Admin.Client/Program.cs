823c26310ef5bb7598142810e18f32faf664328b
Thien Nguyen <thienn@outlook.com>
2025-07-11 16:43:59 +0700

    admin skeleton and authentication

--- NTG.Agent.Admin/NTG.Agent.Admin.Client/Program.cs
+++ NTG.Agent.Admin/NTG.Agent.Admin.Client/Program.cs
@@ -1,5 +1,15 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using NTG.Agent.Admin.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddAuthenticationStateDeserialization();
+
+builder.Services.AddHttpClient<TestClient>(client =>
+{
+    client.BaseAddress = new("https://localhost:7097");
+});
 
 await builder.Build().RunAsync();
