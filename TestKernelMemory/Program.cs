53f7539d5cf5cb21f4daf77814b74bef44108409
Thien Nguyen <thienn@outlook.com>
2025-07-21 15:47:24 +0700

    add kernel memory

---
+++ TestKernelMemory/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.KernelMemory;
+
+Console.WriteLine("Hello, World!");
+
+var memory = new MemoryWebClient("https://localhost:7181", "FfxFT4Twbyv3RBocPY00gXFnaIDt3z3PsYiiChH4eEqhlfsdfsdfsdHv6XJ3w3AAABACOGIgye"); // <== URL of KM web service
+
+// Import a file
+await memory.ImportDocumentAsync("C:\\DATA\\test.docx");
+
+var answer = await memory.SearchAsync("How I can claim for the POV");
+
+Console.WriteLine(answer.Query);
+
+foreach (var x in answer.Results)
+{
+    Console.WriteLine($"  * {x.SourceName} -- {x.Partitions.First().Text}");
+}
+
