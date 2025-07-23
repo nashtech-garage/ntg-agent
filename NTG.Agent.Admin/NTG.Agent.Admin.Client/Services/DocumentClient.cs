d1cc8e608aa491bdeb984d341036d930c31976ed
Thien Nguyen <thienn@outlook.com>
2025-07-23 16:53:27 +0700

    implement get document for agent

---
+++ NTG.Agent.Admin/NTG.Agent.Admin.Client/Services/DocumentClient.cs
@@ -0,0 +1,15 @@
+﻿using NTG.Agent.Shared.Dtos.Documents;
+using System.Net.Http.Json;
+
+namespace NTG.Agent.Admin.Client.Services;
+
+public class DocumentClient(HttpClient httpClient)
+{
+    public async Task<IList<DocumentListItem>> GetDocumentsByAgentIdAsync(Guid agentId)
+    {
+        var response = await httpClient.GetAsync($"api/documents/{agentId}");
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadFromJsonAsync<IList<DocumentListItem>>();
+        return result ?? [];
+    }
+}
--- NTG.Agent.Orchestrator/Controllers/DocumentsController.cs
+++ NTG.Agent.Orchestrator/Controllers/DocumentsController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NTG.Agent.Orchestrator.Data;
+using NTG.Agent.Shared.Dtos.Documents;
 
 namespace NTG.Agent.Orchestrator.Controllers;
 [Route("api/[controller]")]
@@ -10,6 +13,17 @@ public class DocumentsController : ControllerBase
     public DocumentsController(AgentDbContext agentDbContext)
     {
         _agentDbContext = agentDbContext ?? throw new ArgumentNullException(nameof(agentDbContext));
+    }
+
+    [HttpGet("{agentId}")]
+    [Authorize]
+    public async Task<IActionResult> GetDocumentsByAgentId(Guid agentId)
+    {
+        var documents = await _agentDbContext.Documents
+            .Where(x => x.AgentId == agentId)
+            .Select(x => new DocumentListItem (x.Id, x.Name, x.CreatedAt, x.UpdatedAt))
+            .ToListAsync();
+        return Ok(documents);
     }
 
 }
