fc34a557a027f87bee6ced3666b63788f7526acc
Anh Nguyen Sieu <sieunhantanbao@gmail.com>
2025-08-17 11:05:23 +0700

    Implement users - roles management

---
+++ NTG.Agent.Admin/NTG.Agent.Admin/Data/Migrations/20250817003623_AddFirstNameLastNametoUSer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NTG.Agent.Admin.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFirstNameLastNametoUSer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "e0afe23f-b53c-4ad8-b718-cb4ff5bb9f71",
+                columns: new[] { "FirstName", "LastName" },
+                values: new object[] { null, null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
