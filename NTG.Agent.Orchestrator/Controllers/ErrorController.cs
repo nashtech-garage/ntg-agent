7097c577cec038a774190ef0aacc59e23a6ef620
Thien Nguyen <thienn@outlook.com>
2025-10-30 11:26:40 +0700

    clean up

--- NTG.Agent.Orchestrator/Controllers/ErrorController.cs
+++ NTG.Agent.Orchestrator/Controllers/ErrorController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System.Diagnostics.CodeAnalysis;
 
 namespace NTG.Agent.Orchestrator.Controllers;
 
