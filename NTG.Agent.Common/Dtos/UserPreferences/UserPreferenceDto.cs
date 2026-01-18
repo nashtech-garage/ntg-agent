1f348efa9e82fdfa9b397296ef3675f9120f5cdb
Anh Nguyen Sieu <sieunhantanbao@gmail.com>
2026-01-18 15:27:04 +0700

    Implement Memory Preference for user

--- NTG.Agent.Common/Dtos/UserPreferences/UserPreferenceDto.cs
+++ NTG.Agent.Common/Dtos/UserPreferences/UserPreferenceDto.cs
@@ -1,2 +1,7 @@
 namespace NTG.Agent.Common.Dtos.UserPreferences;
-public record UserPreferenceDto(Guid SelectedAgentId);
+
+public record UserPreferenceDto(
+    Guid SelectedAgentId,
+    bool? IsLongTermMemoryEnabled,
+    bool? IsMemorySearchEnabled
+);
